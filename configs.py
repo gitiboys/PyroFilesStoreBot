1fb215b02e9e7718da37c7f26cfaee9a24e71f96
gitiboys <82661142+gitiboys@users.noreply.github.com>
2021-04-18 15:59:00 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -37,7 +37,5 @@ Also remember that developer will Delete Adult Contents from Database. So better
 [Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
 """
 	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
-
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+`I'm no more alive 😕`
 """
